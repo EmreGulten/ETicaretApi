35f1e763a040e351830c4e5730bda38dabe42486
fatal <fatal@EMRE>
2022-07-24 16:44:14 +0300

    Add project files.

---
+++ ETicaret.Data/Concrete/EntityFramework/ContextDb.cs
@@ -0,0 +1,15 @@
+﻿using ETicaret.Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+
+namespace ETicaret.Data.Concrete.EntityFramework
+{
+    public class ContextDb :DbContext
+    {
+        public ContextDb(DbContextOptions options):base(options)
+        {
+
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
---
+++ WebApi/Controllers/ProductsController.cs
@@ -0,0 +1,33 @@
+﻿using ETicaret.Data.Concrete.EntityFramework;
+using ETicaret.Entities.Concrete;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly ContextDb _contextDb;
+
+        public ProductsController(ContextDb contextDb)
+        {
+            _contextDb = contextDb;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Product>>> GetProducts()
+        {
+            var result = await _contextDb.Products.ToListAsync();
+            return result;
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Product>> GetProducts(int id)
+        {
+            return await _contextDb.Products.FindAsync(id);
+        }
+    }
+}
